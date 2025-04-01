50003ed72bc676b3c232c8fd2625498abcd5dd2f
Fathimathuzahra <fz551914@gmail.com>
2025-04-01 15:00:22 +0530

    Add files via upload

---
+++ Q5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Enter first number"
+read a
+echo "Enter first number"
+read b
+echo "Enter ur choice"
+read choice
+if [ $choice == 1 ]
+then
+	echo " Sum of $a and $b is= ` expr $a + $b `"
+elif [ $choice == 2 ]
+then
+        echo " Difference of $a and $b is = ` expr $a - $b `"
+elif [ $choice == 3 ]
+then
+        echo " Product of $a and $b is = ` expr $a \* $b `"
+elif [ $choice == 4 ]
+then
+        echo " Division = ` expr $a / $b `"
+
+fi
