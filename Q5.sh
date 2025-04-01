d2820d25eb321872d2fc9ccc0758debb7fd0543e
Fathimathuzahra <fz551914@gmail.com>
2025-04-01 15:11:34 +0530

    Add files via upload

--- Q5.sh
+++ Q5.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+echo "CALCULATOR"
+
 echo "Enter first number"
 read a
 echo "Enter first number"
 read b
 echo "Enter ur choice"
+echo "1.Addition"
+echo "2.Difference"
+echo "3.Multiplication"
+echo "4.Dividsion"
+echo "5.Modulo division"
 read choice
 if [ $choice == 1 ]
 then
@@ -16,7 +23,11 @@ then
         echo " Product of $a and $b is = ` expr $a \* $b `"
 elif [ $choice == 4 ]
 then
-        echo " Division = ` expr $a / $b `"
+        echo " Division of $a and $b is= ` expr $a / $b `"
+elif [ $choice == 5 ]
+then
+        echo " Modulo division of $a and $b is= ` expr $a % $b `"
+
 else
 	echo "Invalid choice"
 
