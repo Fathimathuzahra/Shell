30119bacd3a78b2c166c438da0763a49421b2ee2
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 22:00:01 +0530

    Update Q 12.sh

--- RECORD/Q 12.sh
+++ RECORD/Q 12.sh
@@ -1,4 +1,3 @@
-Q)Write a shell script program to calculate the factorial of a given number Using Function .
 
 #!/bin/bash
 clear
@@ -21,18 +20,3 @@ Output:
 Enter a number : 5
 Factorial of 5 is: 120
 
-
-------------------------------------
-
-
-Algorithm:
-
-Factorial :
-Step 1: Initialize f to 1 and i to 1.
-Step 2: Use a loop to multiply f by i until i reaches the input number.
-Step 3: Return the value of f.
-
-Main :
-Step 1: Ask the user to enter a number and store it in variable n.
-Step 2: Call the factorial function with n as the argument.
-Step 3: Display the result returned by the factorial function.
