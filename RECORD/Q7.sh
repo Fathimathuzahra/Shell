31a52eaa8bf5f59a1604b943c74c266252eb7286
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:44:07 +0530

    Update Q7.sh

--- RECORD/Q7.sh
+++ RECORD/Q7.sh
@@ -1,6 +1,5 @@
-Q)Write a shell script to print even numbers in a range.   
 
-
+Code:
 #!/bin/bash
 clear
 read -p "Enter the range : " i n
@@ -15,18 +14,6 @@ done
 
 
 Output:
-Enter the range : 1 10
-Even numbers :  2 4 6 8 10
+Enter the range : 1 9
+Even numbers :  2 4 6 8
 
-
-
-
-
-
-Algorithm:
-Step 1: Ask the user to enter a range, with i as the starting value and n as the ending value.
-Step 2: Store the inputs in variables i and n.
-Step 3: Print the label "Even numbers".
-Step 4: Use a loop to iterate from i to n.
-Step 5: Inside the loop, check if the current number is even.
-Step 6: If true, print the number.
