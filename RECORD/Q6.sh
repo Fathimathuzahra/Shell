632107b25853f1437e2ec3178f0bd25eef07843a
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:45:39 +0530

    Update Q6.sh

--- RECORD/Q6.sh
+++ RECORD/Q6.sh
@@ -1,6 +1,4 @@
-Q)Write a shell script to test if three numbers form a triangle.    
-
-
+Code:
 #!/bin/bash
 clear
 read -p "Enter the 3 sides of triangle : " a b c
@@ -15,14 +13,3 @@ fi
 Output:
 Enter the 3 sides of triangle : 5 10 15
 The triangle cannot be formed
-
-
-
-
-
-Algorithm:
-Step 1: Ask the user to enter the three sides of the triangle.
-Step 2: Store the inputs in variables a, b, and c.
-Step 3: Check if the sum of any two sides is less than or equal to the third side.
-Step 4: If true, print that the triangle cannot be formed.
-Step 5: If false, print that the triangle can be formed.
