7a45af135cff1e9c081a1c61fb2076ffdd46657e
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:46:27 +0530

    Update Q1.sh

--- RECORD/Q1.sh
+++ RECORD/Q1.sh
@@ -1,5 +1,4 @@
-Q)Write a shell script program to display a given message.
-
+Code:
 #!/bin/bash
 clear
 read -p "Enter a String : " a
@@ -9,10 +8,3 @@ echo "$a"
 Output:
 Enter a String : HELLO
 HELLO
-
-
-
-Algorithm:
-Step 1: Ask the user to enter a string.
-Step 2: Store the input in a variable.
-Step 3: Display the string.
