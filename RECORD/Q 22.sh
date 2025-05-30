08ba0baaf8923c75e89a39acf0c738c3d05fe59b
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:39:41 +0530

    Update Q 22.sh

--- RECORD/Q 22.sh
+++ RECORD/Q 22.sh
@@ -1,5 +1,3 @@
-Q)Shell script to count the occurrence of words in a file in a set of files
-
 
 Code:
 
@@ -24,16 +22,6 @@ shift
 done
 echo "no.of occurance of words = $count"
 
-Algorithm:
-Step 1: If no args given, show syntax and exit.
-Step 2: Set word to first arg, then remove it from args.
-Step 3: Set count to 0.
-Step 4: For each file in args:
- – Read each word in the file.
- – If word matches word, increase count by 1.
-Step 5: Move to next file until done.
-Step 6: Print total count of occurrences.
-Step 7: End.
 
 
 Input:
@@ -43,11 +31,10 @@ vi file1.txt
  ----------------------file1.txt--------------------
  Hello good morning!
  This is the first line
- This is the second line
 -------------------------------------------------------
 
 sh wordoccurance.sh This file1.txt 
 
 
 Output:
-no.of occurance of words = 2
+no.of occurance of words = 8
