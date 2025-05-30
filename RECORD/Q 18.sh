39dd0a0dcb65b188f0696e45d4e738f1e3b5d436
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:21:20 +0530

    Update Q 18.sh

--- RECORD/Q 18.sh
+++ RECORD/Q 18.sh
@@ -1,8 +1,3 @@
-Q)Shell script to delete all lines if a file containing word linux
-
-
-Code:
-
 if [ $# -ne 2 ]
 then
         echo " syntax is<$0><string><filename>"
@@ -23,16 +18,6 @@ exec<$term
 mv temp $2
 
 
-Algorithm:
-Step 1: If args ≠ 2, show syntax and exit.
-Step 2: Save current terminal name to term.
-Step 3: Redirect input from the given file.
-Step 4: For each line: check if it has the exact word.
-Step 5: If not matched exactly, add line to temp.
-Step 6: Restore input back to terminal.
-Step 7: Replace original file with temp.
-Step 8: End.
-
 
 Input:
 --------------------------file3.txt----------------------
@@ -40,7 +25,7 @@ This line contains the word linux
 Line without the word
 
 ----------------------------------------------------------------
-sh linux.sh file3.txt
+sh linux.sh linux file3.txt
 
 
 Output:
