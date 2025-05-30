5fde2935683cff34180cbb5f90395e7c3b0b2354
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:29:32 +0530

    Update Q 19.sh

--- RECORD/Q 19.sh
+++ RECORD/Q 19.sh
@@ -18,8 +18,3 @@ done
 Input:
 sh window.sh file1.txt
 
-Output:
- Directory of /c/Users/MCA19/Desktop/New folder
-
-A.SH.1610                225     22-05-25 13:35:05
-b.sh                     265     22-05-25 14:09:00
