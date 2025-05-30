fc78ff4abceea2666aee1b07520a3a0b2b4f7c2b
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:48:57 +0530

    Update Q3.sh

--- RECORD/Q3.sh
+++ RECORD/Q3.sh
@@ -1,5 +1,4 @@
-Q)Write a shell script program to check the largest among three numbers.
-
+Code:
 #!/bin/bash
 clear
 read -p "Enter 3 numbers : " a b c
@@ -17,11 +16,3 @@ fi
 Output:
 Enter 3 numbers : 5 10 15
 15 is the largest
-
-Algorithm:
-Step 1: Ask the user to enter three numbers.
-Step 2: Store the inputs in variables a, b, and c.
-Step 3: Compare a with b and c to check if a is the largest.
-Step 4: If a is the largest, print that a is the largest.
-Step 5: If b is larger than c, print that b is the largest.\
-Step 6: If neither of the above conditions is true, print that c is the largest.
