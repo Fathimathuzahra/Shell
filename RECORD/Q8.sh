ac836ee5beb85ce5dd2304f6d13f8cfcea9faea8
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:43:01 +0530

    Update Q8.sh

--- RECORD/Q8.sh
+++ RECORD/Q8.sh
@@ -1,6 +1,4 @@
-Q)Write a shell script to check if a number is prime. 
-
-
+Code: 
 #!/bin/bash
 clear
 read -p "Enter the value : " p
@@ -23,17 +21,3 @@ fi
 Output:
 Enter the value : 5
 5 is a prime number
-
-
-
-
-
-
-
-Algorithm:
-Step 1: Ask the user to enter a value and store it in variable p.
-Step 2: Check if p is equal to 1.
-If true, print that p is neither prime nor composite.
-Step 3: If p is not 1, use a loop to check if p is divisible by any number from 2 to p/2.
-Step 4: If p is divisible by any number, print that p is not a prime number and exit.
-Step 5: If no divisor is found, print that p is a prime number.
