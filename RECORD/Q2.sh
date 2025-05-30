ab764aeab98c779fa59be30f23a46990619cd784
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:48:05 +0530

    Update Q2.sh

--- RECORD/Q2.sh
+++ RECORD/Q2.sh
@@ -1,5 +1,4 @@
- Q)Write a shell script to check whether the given number is odd or even.  
-
+Code:
 #!/bin/bash
 clear
 read -p "Enter the value : " a
@@ -14,12 +13,3 @@ fi
 Output:
 Enter the value : 5
 5 is odd
-
-
-
-Algorithm:
-Step 1: Ask the user to enter a value.
-Step 2: Store the input in a variable a.
-Step 3: Check if a is divisible by 2 (even).
-Step 4: If true, print that the number is even.
-Step 5: If false, print that the number is odd.
