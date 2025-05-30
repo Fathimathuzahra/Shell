25968d439da08bf10bcc1b8bf56c3d968185807e
Fathimathuzahra <fz551914@gmail.com>
2025-05-30 21:56:19 +0530

    Update Q 13.sh

--- RECORD/Q 13.sh
+++ RECORD/Q 13.sh
@@ -1,5 +1,3 @@
-Q)Write a shell script program that displays a different message based on the current time of the day.  
-
 
 #!/bin/bash
 clear
@@ -16,13 +14,4 @@ else
 fi
 
 Output:
-Good Morning!
-
-
-Algorithm:
-Step 1: Get the current hour using the date command and store it in the variable hour.
-Step 2: Check if the hour is between 5 and 12 (morning).
-If true, print "Good Morning!".
-Step 3: If the hour is between 12 and 18 (afternoon), print "Good Afternoon!".
-Step 4: If the hour is between 18 and 21 (evening), print "Good Evening!".
-Step 5: If none of the above conditions are true, print "Good Night!".
+Good Afternoon!
